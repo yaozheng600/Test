d91dcc2a8c3b8f0b6239b17b42d2d56a1fd00dff
yaozheng600 <yaozheng_levi@163.com>
2023-09-28 19:21:10 +0200

    add a test by yao
    Signed-off-by: yaozheng600 <yaozheng_Levi@163.com>

---
+++ main.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import gettext
+import locale
+
+_ = gettext.gettext
+current_locale = locale.getlocale()
+locale.setlocale(locale.LC_ALL,current_locale)
+localizor_DE = gettext.translation('base',localedir='locale',languages=['DE'])
+localizor_DE.install()
+localizor_EN = gettext.translation('base',localedir='locale',languages=['EN'])
+localizor_EN.install()
+if st.button("DE"):
+    _ = localizor_DE.gettext
+if st.button("EN"):
+    _ = localizor_EN.gettext
+if st.button("Original"):
+    _ = lambda s:s
+st.title(_("标题"))
+x="引用"
+st.button(_("f函数里的button{x}"))
+st.header(_("副标题"))
+st.markdown(_("写在write里的文字。"))
+st.markdown(_("markdown里的文字"))
