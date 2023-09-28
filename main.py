96045c16684692b0b4f232ad2cc3ce8b3cbe0a8c
yaozheng600 <yaozheng_Levi@163.com>
2023-09-28 20:28:42 +0200

    add a test by yao
    Signed-off-by: yaozheng600 <yaozheng_Levi@163.com>

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import gettext
 
 
 _ = gettext.gettext
-localizor_DE = gettext.translation('base',localedir='./locale',languages=['DE'])
+localizor_DE = gettext.translation('base',localedir='https://github.com/yaozheng600/Test/blob/fe45434aa985780e3ba09d7b336992dcc0df6c44/locale',languages=['DE'])
 localizor_DE.install()
-localizor_EN = gettext.translation('base',localedir='./locale',languages=['EN'])
+localizor_EN = gettext.translation('base',localedir='https://github.com/yaozheng600/Test/blob/fe45434aa985780e3ba09d7b336992dcc0df6c44/locale',languages=['EN'])
 localizor_EN.install()
 if st.button("DE"):
     _ = localizor_DE.gettext
