c3d4e8b4cb6c63a2f24f6ab9d07aa7009ff59aea
yaozheng600 <yaozheng_Levi@163.com>
2023-09-29 09:40:03 +0200

    add a test by yao
    Signed-off-by: yaozheng600 <yaozheng_Levi@163.com>

--- main.py
+++ main.py
@@ -3,21 +3,27 @@ import pandas as pd
 import gettext
 import os
 
-# 设置 LC_MESSAGES 环境变量
-os.environ['LC_MESSAGES'] = 'DE'
+
+# 获取所有环境变量
+env_vars = os.environ
+
+# 打印所有环境变量及其值
+for key, value in env_vars.items():
+    st.write(f"{key}: {value}")
+
 
 
 _ = gettext.gettext
 
-localizor_DE = gettext.translation('base',localedir='locale',languages=['DE'])
+localizor_DE = gettext.translation('base',localedir='locale',languages=['de'])
 localizor_DE.install()
-localizor_EN = gettext.translation('base',localedir='locale',languages=['EN'])
+localizor_EN = gettext.translation('base',localedir='locale',languages=['en'])
 localizor_EN.install()
 
 st.dataframe(pd.read_csv('Sleep_health_and_lifestyle_dataset.csv'))
-if st.button("DE"):
+if st.button("de"):
     _ = localizor_DE.gettext
-if st.button("EN"):
+if st.button("en"):
     _ = localizor_EN.gettext
 if st.button("Original"):
     _ = lambda s:s
